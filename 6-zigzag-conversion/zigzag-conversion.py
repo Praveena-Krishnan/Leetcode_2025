358669799e75dc42e5006a1e28c319f2fae29d2a
Praveena Krishnan <141300671+Praveena-Krishnan@users.noreply.github.com>
2025-09-28 23:18:01 +0530

    Time: 9 ms (70.96%) | Memory: 17.8 MB (69.70%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,25 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows==1 or numRows>=len(s):
+            return s
+
+        rows=[[] for row in range(numRows)]
+        index=0 #which row we are in
+        step=1 # up row/down row
+        for char in s:
+            rows[index].append(char)
+            if index==0:
+                step=1
+            elif index== numRows-1:
+                step=-1
+            index+=step
+        
+        for i in range(numRows):
+            rows[i]="".join(rows[i])
+        return "".join(rows)
+
+
+
+        
+
+        
